5fa16ff63d8c7a2deb1694a4fffdde1068c14fa8
Parham zandnia <136172071+PMZ80@users.noreply.github.com>
2023-07-14 05:32:57 -0700

    secend-commit
    add typewrite in the header

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+var i = 0;
+var txt = ' Parham Zandnia';
+var speed = 90;
 
-
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("txt-2").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
